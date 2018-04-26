2856c28072c3f703882b41684f1ca4c71b8cd156
tolribeiro <thiago@toribeiro.com>
2018-04-26 12:34:52 -0500

    Component basics @Components

--- fundamentals/seed-project/app/app.component.ts
+++ fundamentals/seed-project/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
 
+// 'Component' -> decorator
+// The Component is a function, but as it has a decorator
+// which is a TS feature, to register with Angular just use @
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  selector: 'app-root', // creates the element in the HTML
+  styleUrls: ['app.component.scss'], // array that contains a reference to a scss file
   template: `
-    <div class="app">
-      Hello!
+    <div class='app'>
+    {{ title }} 
     </div>
-  `
+  ` // ES6 feature that allows multiple line strings
+  // We can also use a templateUrl and reference the file
 })
-export class AppComponent {
 
+// Interpolation {{}}
+
+// It holds metadata for a particular component (in this case App Component)
+
+export class AppComponent {
+  // We can do just that, since we are using TS
+  title: string; // setting the static property
+  // However, we could also do
+  constructor() {
+    this.title = 'Angular Lab';
+  }
 }
